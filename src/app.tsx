c1764931edca4cbe599e71e30682d0d2d0c557b8
Austin Kindred <austin.kindred@gmail.com>
2025-10-21 16:57:40 -0400

    [styles] MVP for mobile styling

--- src/app.tsx
+++ src/app.tsx
@@ -13,15 +13,18 @@ function App() {
             </Helmet>
             <div className="h-full w-full overflow-hidden flex flex-col text-xl dark">
                 <div className="bg-slate-900 flex-0">
-                    <div className="px-4 pb-4 pt-0.5 flex flex-row text-white font-think-loved items-end">
+                    <div className="px-4 pb-4 mt-4 flex flex-row text-white font-think-loved items-center">
                         <div
-                            className="mt-2 mr-2 w-20 h-20 rounded-full inline-block bg-cover bg-center"
+                            className="mr-2 w-10 h-10 md:w-20 md:h-20 rounded-full inline-block bg-cover bg-center"
                             style={{ backgroundImage: `url('/sumologo.png')` }}
                         />
-                        <span className="text-6xl bg-linear-to-r from-cyan-400 to-indigo-500 bg-clip-text text-transparent">
+                        <span className="text-2xl md:text-4xl lg:text-6xl bg-linear-to-r from-cyan-400 to-indigo-500 bg-clip-text text-transparent">
                             Get Scwhifty
                         </span>
-                        <span className="text-6xl">&nbsp;Football</span>
+                        <span className="block md:hidden text-2xl">&nbsp;FF</span>
+                        <span className="hidden md:block md:text-4xl lg:text-6xl">
+                            &nbsp;Football
+                        </span>
                     </div>
                     <NavigationBar />
                 </div>
